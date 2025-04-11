b0cddcf2ed1fcb42a43a2669ca890d3ded981f29
charroux <benoit.charroux@gmail.com>
2025-04-11 11:48:58 +0200

    newcarservice

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class VoitureTest {
 
     @Test
     void creerVoiture(){
-
+        Voiture v = new Voiture("Ferrari", 100);
+        assertEquals(v.getMarque(), "Clio");
     }
 
 }
