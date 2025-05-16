2abad054f9ccf71a9cacb4e54dc388e5f8e1b34d
DevKosX <kosbarmohamed.31@example.com>
2025-05-16 10:10:37 +0200

    Test TP1 COMPLET

--- src/main/java/com/example/demo/web/StatistiqueController.java
+++ src/main/java/com/example/demo/web/StatistiqueController.java
@@ -4,27 +4,45 @@ import com.example.demo.data.Voiture;
 import com.example.demo.service.Echantillon;
 import com.example.demo.service.Statistique;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
-@RestController
+
+@Controller // <-- NE PAS utiliser @RestController
 public class StatistiqueController {
 
     @Autowired
     Statistique statistique;
 
-    @GetMapping(value = "/statistique")
+    @GetMapping("/page-voiture")
+    public String afficherPageVoiture() {
+        return "voiture"; // => templates/voiture.html
+    }
+
+    @PostMapping("/voiture")
+    @ResponseBody // cette méthode renvoie du JSON
+    public void creerVoiture(@RequestBody Voiture voiture) {
+        statistique.ajouter(voiture);
+    }
+
+    @GetMapping("/statistique")
+    @ResponseBody // cette méthode renvoie du JSON
     public Echantillon getStatistiques() throws PasDeVoitureException {
-        try{
+        try {
             return statistique.prixMoyen();
         } catch (ArithmeticException e) {
             throw new PasDeVoitureException();
         }
-
     }
 
-    @PostMapping("/voiture")
-    public void creerVoiture(@RequestBody Voiture voiture){
-        statistique.ajouter(voiture);
+    @GetMapping("/liste-voitures")
+    public String afficherListeVoitures(Model model) {
+        model.addAttribute("voitures", statistique.getVoitures());
+        return "liste-voitures"; // => templates/liste-voitures.html
     }
 
 }
--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -1,17 +1,40 @@
 package com.example.demo.data;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
 public class VoitureTest {
 
-    @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    private Voiture voiture;
+
+    @BeforeEach
+    void initialiserVoiture() {
+        voiture = new Voiture(); //initialisation pour chaque test
     }
 
+    @Test
+    void creerVoitureRenault() {
+        voiture.setMarque("Renault");
+        voiture.setPrix(12000);
+
+        assertEquals("Renault", voiture.getMarque());
+        assertEquals(12000, voiture.getPrix());
+    }
+
+    @Test
+    void creerVoitureAudi() {
+        voiture.setMarque("Audi");
+        voiture.setPrix(22000);
+
+        assertEquals("Audi", voiture.getMarque());
+        assertEquals(22000, voiture.getPrix());
+    }
+
+    @Test
+    void modifierIdVoiture() {
+        voiture.setId(10);
+        assertEquals(10, voiture.getId());
+    }
 }
