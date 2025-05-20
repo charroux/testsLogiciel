151be99534df33221ca400a0fa66052c414adb5a
GraceKoffi <grace01koffi@gmail.com>
2025-05-20 10:19:29 +0200

    20/05 - update voitureTest

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -9,9 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 public class VoitureTest {
 
+    private final Voiture v1 = new Voiture();
+
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+
+        v1.setId(204);
+        v1.setMarque("Maza");
+        v1.setPrix(600000);
+
+        assertEquals(204, v1.getId());
+        assertEquals("Maza", v1.getMarque());
+        assertEquals(600000, v1.getPrix());
+
     }
 
 }
