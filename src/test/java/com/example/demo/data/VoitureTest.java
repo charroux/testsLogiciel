0d8680e5fed08cb8f6303a7b2c5ac71eaaa2d15e
DevKosX <kosbarmohamed.31@example.com>
2025-05-22 14:44:38 +0200

    Ajout de commentaire pour décrire

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -8,31 +8,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VoitureTest{
 
     private Voiture voiture;
-
+    /** L'annotation BeforeEach permet d'initialser
+     *
+     */
+    /**une voiture commune pour chaque test*/
     @BeforeEach
     void initialiserVoiture(){
-        voiture = new Voiture(); //initialisation pour chaque test
+        voiture = new Voiture(); /** initialisation pour chaque test */
     }
-
+    /**Annotaion de test*/
     @Test
     void TestVoitureRenault(){
+        /** On enregistre ces première valeurs au newVoiture() vide*/
         voiture.setMarque("Renault");
         voiture.setPrix(12000);
 
-        assertEquals("Renault", voiture.getMarque()); //test equals avec la marque
+        /** On vérifie si les valeurs sont ceux enregistrés*/
+        assertEquals("Renault", voiture.getMarque());
         assertEquals(12000, voiture.getPrix());
     }
 
     @Test
     void TestVoitureAudi(){
-        voiture.setMarque("Audi"); //2eme test
+
+        /** On modifie setMarque et SetPrix - Enregistrement */
+        voiture.setMarque("Audi");
         voiture.setPrix(22000);
 
+        /** On vérifie si les valeurs sont ceux enregistrés encore */
         assertEquals("Audi", voiture.getMarque());
         assertEquals(22000, voiture.getPrix());
     }
 
     @Test
+    /**On va tester si le test assertEquals marche pour le champ id*/
     void modifierIdVoiture(){
         voiture.setId(10);
         assertEquals(10, voiture.getId());
