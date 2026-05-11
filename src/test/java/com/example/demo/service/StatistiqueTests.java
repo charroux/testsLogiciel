7e8bd035700acc5001a15f1e782a2baa04a1619b
charroux <benoit.charroux@gmail.com>
2026-05-11 10:43:04 +0200

    test

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -9,25 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StatistiqueTests {
 
-    @Test
-    void ajouterEtCalculerPrixMoyen() {
-        StatistiqueImpl statistiqueImpl = new StatistiqueImpl();
-
-        statistiqueImpl.ajouter(new Voiture("Renault", 12000));
-        statistiqueImpl.ajouter(new Voiture("Peugeot", 18000));
-        statistiqueImpl.ajouter(new Voiture("Toyota", 15000));
-
-        Echantillon echantillon = statistiqueImpl.prixMoyen();
-
-        assertEquals(3, echantillon.getNombreDeVoitures());
-        assertEquals(15000, echantillon.getPrixMoyen());
-    }
-
-    @Test
-    void prixMoyenSansVoitureDeclencheUneException() {
-        StatistiqueImpl statistiqueImpl = new StatistiqueImpl();
-
-        assertThrows(ArithmeticException.class, statistiqueImpl::prixMoyen);
-    }
+    
 
 }
